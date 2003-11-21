bc4943c75bc799edd5214deee60cf9f28fc05e61
Sleepycat Software <db@sleepycat.com>
2003-11-21 03:32:54 +0000

    Sleepycat Software: Berkeley DB 4.2.50: (November 20, 2003)
    Downloaded from https://ftp.mizar.org/packages/db/db-4.2.50.tar.gz
    matches https://download.oracle.com/berkeley-db/db-4.2.50.tar.gz.md5
    7cc0c4dc4a666acc8d39714fe2f3a255  db-4.2.50.tar.gz

---
+++ java/src/com/sleepycat/db/DbClient.java
@@ -0,0 +1,17 @@
+/*
+ *  -
+ *  See the file LICENSE for redistribution information.
+ *
+ *  Copyright (c) 2000-2003
+ *  Sleepycat Software.  All rights reserved.
+ *
+ *  $Id: DbClient.java,v 11.13 2003/11/10 16:22:59 bostic Exp $
+ */
+package com.sleepycat.db;
+
+/**
+ *  The DbClient object is used to encapsulate a reference to an RPC client.</p>
+ *
+ */
+public interface DbClient {
+}
